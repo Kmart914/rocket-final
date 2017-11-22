186dcdf3c084496cae06ac1dca6549ab4d07cae5
Kevin Martzolf <kmart914@gmail.com>
2017-11-21 18:23:27 -0800

    switched app file and added bod

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Nav from './Nav';
-import Bod from './Bod';
 import registerServiceWorker from './registerServiceWorker';
+import App from './App';
 
-ReactDOM.render(<Nav />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
